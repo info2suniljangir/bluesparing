a38bc28ab3e8bb1b7852e845d11c4a5efee9983a
Sunil Jangir <info2suniljangir@gmail.com>
2024-12-27 17:37:03 +0530

    fixing img

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["bluesparing.vercel.app"],
+    
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "bluesparing.vercel.app",
-      },
+      
     ],
   },
 

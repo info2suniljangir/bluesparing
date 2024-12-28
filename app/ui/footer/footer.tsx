f9b5f41b6ea63e804eda962693b04d3ab8023725
Sunil Jangir <info2suniljangir@gmail.com>
2024-12-28 13:51:39 +0530

    schroll bar fixed

--- app/ui/footer/footer.tsx
+++ app/ui/footer/footer.tsx
@@ -28,9 +28,8 @@ const Footer = () => {
               We are the best IT software provider to improve your work.
               Specializing in insurance IT solutions, we enhance efficiency and
               customer engagement through innovative app development.
-            </div >
-           
-            
+            </div>
+
             <div></div>
           </div>
           {/* Quick Links */}
@@ -40,17 +39,25 @@ const Footer = () => {
                 Quick Links
               </h3>
             </div>
-            <Link href='#about'>
-            <div className="text-[13px] leading-7 hover:text-customOrange">About</div>
+            <Link href="#about">
+              <div className="text-[13px] leading-7 hover:text-customOrange">
+                About
+              </div>
             </Link>
-            <Link href='#service'>
-            <div className="text-[13px] leading-7 hover:text-customOrange">Services</div>
+            <Link href="#service">
+              <div className="text-[13px] leading-7 hover:text-customOrange">
+                Services
+              </div>
             </Link>
-            <Link href='#blogs'>
-            <div className="text-[13px] leading-7 hover:text-customOrange">Blogs</div>
+            <Link href="#blogs">
+              <div className="text-[13px] leading-7 hover:text-customOrange">
+                Blogs
+              </div>
             </Link>
-            <Link href='#testimonial'>
-            <div className="text-[13px] leading-7 hover:text-customOrange">Testimonials</div>
+            <Link href="#testimonial">
+              <div className="text-[13px] leading-7 hover:text-customOrange">
+                Testimonials
+              </div>
             </Link>
           </div>
           {/* Contact Info */}
@@ -76,7 +83,11 @@ const Footer = () => {
             </div>
 
             <div className="my-5">
-              <input type="email" placeholder="Email Address" className="h-11 rounded-tl-[4px] rounded-bl-[4px] outline-none text-black p-[10px] text-sm w-[80%]"/>
+              <input
+                type="email"
+                placeholder="Email Address"
+                className="h-11 rounded-tl-[4px] rounded-bl-[4px] outline-none text-black p-[10px] text-sm w-[80%]"
+              />
               <button className="bg-customOrange absolute h-11 p-4 rounded-tr-[4px] rounded-br-[4px] text-white">
                 <FaPaperPlane />
               </button>
@@ -89,27 +100,39 @@ const Footer = () => {
         <div className="border-[1px] border-b-[#d6c4c4] w-full my-8"></div>
         {/* Bottom Section */}
         <div className="text-center my-4 flex justify-between items-center">
-            <div className="text-xs font-normal"> © 2024 Work Field. All rights reserved. Design by❤️ Blue Sparing</div>
-            <div className="text-xs font-bold text-customOrange">TOP WEB AND APP DESIGN COMPANY IN INDIA</div>
-             {/* Social media logos */}
-             <div className="flex gap-4 ">
-            <div className="font-bold text-[32px] pt-[2px] hover:text-customOrange">
-              <PiFacebookLogoBold />
-            </div>
-            <div className="font-bold text-4xl hover:text-customOrange">
-              <TiSocialLinkedinCircular />
-            </div>
-            <div className="font-bold text-4xl hover:text-customOrange">
-              <TiSocialTwitterCircular />
-            </div>
-            <div className="font-bold text-4xl hover:text-customOrange">
-              <TiSocialGooglePlusCircular />
-            </div>
+          <div className="text-xs font-normal">
+            {" "}
+            © 2024 Work Field. All rights reserved. Design by❤️ Blue Sparing
+          </div>
+          <div className="text-xs font-bold text-customOrange">
+            TOP WEB AND APP DESIGN COMPANY IN INDIA
+          </div>
+          {/* Social media logos */}
+          <div className="flex gap-4 ">
+            <Link href="/">
+              <div className="font-bold text-[32px] pt-[2px] hover:text-customOrange">
+                <PiFacebookLogoBold />
+              </div>
+            </Link>
+            <Link href="/">
+              <div className="font-bold text-4xl hover:text-customOrange">
+                <TiSocialLinkedinCircular />
+              </div>
+            </Link>
+            <Link href="/">
+              <div className="font-bold text-4xl hover:text-customOrange">
+                <TiSocialTwitterCircular />
+              </div>
+            </Link>
+            <Link href="/">
+              <div className="font-bold text-4xl hover:text-customOrange">
+                <TiSocialGooglePlusCircular />
+              </div>
+            </Link>
             <div className="font-bold text-4xl hover:text-customOrange">
               <DiGithub />
             </div>
-            </div>
-         
+          </div>
         </div>
       </div>
     </div>
